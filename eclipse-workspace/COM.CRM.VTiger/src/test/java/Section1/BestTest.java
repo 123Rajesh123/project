af941482f425f5c82214d5e0e8144f7b22aed660
123Rajesh123 <rajeshkumar681991@gmail.com>
2023-12-27 16:41:26 +0530

    hiii

---
+++ eclipse-workspace/COM.CRM.VTiger/src/test/java/Section1/BestTest.java
@@ -0,0 +1,14 @@
+package Section1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BestTest {
+
+	@Test
+	public void manas() {
+	Reporter.log("I am you",true);
+	}
+}
+	
+
---
+++ eclipse-workspace/COM.CRM.VTiger/src/test/java/Section1/Class.java
@@ -0,0 +1,13 @@
+package Section1;
+
+import org.junit.Test;
+import org.testng.Reporter;
+
+public class Class {
+@Test
+public void manas() {
+	//System.out.println("I am rajesh");
+	  //or
+	Reporter.log("I am rajesh" , true);
+}
+}
